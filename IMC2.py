e0eff764bee8ff8d93646abd15f89438419bb4fc
Gustavo Henrique de Morais Martins <moraisgustavohenrique02@gmail.com>
2025-06-09 16:40:25 -0300

    fix: tratamentos de erros

--- IMC2.py
+++ IMC2.py
@@ -1,6 +1,6 @@
 try:
-    nome = input("Digite seu nome: ").strip()
-    if not nome:
+    nome_do_paciente = input("Digite seu nome: ").strip()
+    if not nome_do_paciente:
         raise ValueError("Erro: Nome não pode ser vazio.")
 
     peso = float(input("Digite seu peso: "))
@@ -15,7 +15,7 @@ try:
         raise ValueError("Erro: Os valores de altura ou peso parecem irreais.")
 
     imc = peso / (altura * altura)
-    print(f"Nome: {nome} \nIMC: {imc:.2f}")
+    print(f"Nome: {nome_do_paciente} \nIMC: {imc:.2f}")
 
     if imc < 18.5:
         print("Abaixo do peso")
@@ -34,3 +34,20 @@ except ValueError as e:
     print(e)
 except ZeroDivisionError:
     print("Erro: A altura não pode ser zero.")
+
+with open("cadastro_imc_lista.txt" , "a")  as arquivo:
+    arquivo.write(f"\nNome: {nome_do_paciente}\nPeso: {peso}\nAltura: {altura}\nIMC: {imc}\n")
+    
+    if imc < 18.5:
+        arquivo.write("Abaixo do peso")
+    elif 18.5 <= imc <= 24.9:
+        arquivo.write("Peso Normal")
+    elif 25.0 <= imc < 29.9:
+        arquivo.write("Sobrepeso")
+    elif 30 <= imc <= 34.9:
+        arquivo.write("Obesidade Grau I")
+    elif 35 <= imc <= 39.9:
+        arquivo.write("Obesidade Grau II")
+    else:
+        print("Obesidade Grau III")
+    arquivo.write("\n----------------------------------------------------------------------\n")
